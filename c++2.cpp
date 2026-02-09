25c3133f86061dac6972f6eb668c701138ac08f9
LZAzs <anbao0909@outlook.com>
2026-02-09 15:14:41 +0800

    Add files via upload

---
+++ c++2.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+int djg=0;
+int main()
+{
+	for (int i = 1; i <= 100; i++) 
+	{
+		if (i % 2 == 0 && djg % 5 != 0) 
+		{
+			std::cout << i << " ";
+			djg++;
+		}
+		else if(i % 2 == 0 && djg % 5 == 0) 
+		{
+			if (djg == 0) 
+			{
+				std::cout << i << " ";
+			}
+			else 
+			{
+				std::cout <<"\n";
+				std::cout << i << " ";
+			}
+			djg++;
+		}
+		
+	}
+	return 0;
+}
