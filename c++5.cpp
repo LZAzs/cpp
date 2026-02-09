93e158ab8474c84fc89a81f1d683caa5c51073ab
LZAzs <anbao0909@outlook.com>
2026-02-09 15:48:51 +0800

    Add files via upload

---
+++ c++5.cpp
@@ -0,0 +1,16 @@
+﻿#include <algorithm>
+#include<stdio.h>
+#include<iostream>
+int a[10],zh;
+double pjz;
+int main()
+{
+	for (int i = 0; i < 10; i++) {
+		std::cin >> a[i];
+		zh += a[i];
+	}
+	std::sort(a, a + 9);
+	pjz = zh / 10;
+	printf("%d %.1f %d %d", zh, pjz, a[0], a[9]);
+	return 0;
+}
