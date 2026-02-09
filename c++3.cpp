83810d2ed9c82912998f9295f3d723aa6403ea07
LZAzs <anbao0909@outlook.com>
2026-02-09 15:22:58 +0800

    Add files via upload

---
+++ c++3.cpp
@@ -0,0 +1,19 @@
+﻿﻿#include<iostream>
+int n;
+bool prime(int a) {
+	if (a == 0) {
+		return false;
+	}
+	for (int i = 2; i < sqrt(a); i++) {
+		if (a % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	std::cin >> n;
+	std::cout << prime(n) << std::endl;
+	return 0;
+}
